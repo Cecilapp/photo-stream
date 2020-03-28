79d0e2cf5f6500be7be5fe32ae8fff6491253d6b
Arnaud Ligny <arnaud@ligny.org>
2020-03-28 22:53:53 +0100

    Update build.sh

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,4 +1,4 @@
-echo "### Downloading Cecil"
+echo "Downloading Cecil"
 if [ -z $CECIL_VERSION ]; then
   curl -sSOL https://cecil.app/cecil.phar
 else
@@ -6,12 +6,12 @@ else
 fi
 php cecil.phar --version
 
-echo "### Started Cecil build"
+echo "Started Cecil build"
 if [[ $1 == "preview" ]]; then
   php cecil.phar build -v --baseurl=$DEPLOY_PRIME_URL --drafts
 else
   php cecil.phar build -v --baseurl=$URL
 fi
-echo "Finished Cecil build"
 
+echo "Finished Cecil build"
 exit 0
