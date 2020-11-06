9ca202477f490fb66441eda42902bf593f8b6352
Arnaud Ligny <arnaud@ligny.org>
2020-11-06 22:26:02 +0100

    Update Netlify build script

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -8,10 +8,12 @@ php cecil.phar --version
 
 echo "Started Cecil build"
 if [[ $1 == "preview" ]]; then
-  php cecil.phar build -vv --baseurl=$DEPLOY_PRIME_URL --drafts
+  php cecil.phar build -v --baseurl=$DEPLOY_PRIME_URL --drafts
 else
   php cecil.phar build -v --baseurl=$URL
 fi
 
-echo "Finished Cecil build"
-exit 0
+# build success? can deploy?
+if [ $? = 0 ]; then echo "Finished Cecil build"; exit 0; fi
+
+echo "Interrupted Cecil build"; exit 1
